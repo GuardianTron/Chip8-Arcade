631b05facd458e0da1c1559830f3e33af7eb0aa4
Anthony Feeney <VigilanteNighthawk@gmail.com>
2021-02-20 18:30:00 -0500

    Added basic game table to manage games.  Table will likely have more columns added.

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_security.models import fsqla_v2 as fsqla
+from sqlalchemy.orm import backref
 
 db = SQLAlchemy()
 
@@ -24,3 +25,10 @@ class Role(db.Model,fsqla.FsRoleMixin):
     role = db.Column(db.String(80),unique=True)
     description = db.Column(db.Text)
 
+class Game(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    title = db.Column(db.String(255),nullable=False)
+    description = db.Column(db.Text)
+    filename = db.Column(db.String(255),nullable=False)
+    user = db.relationship('User',backref=db.backref('games',lazy='dynamic'))
