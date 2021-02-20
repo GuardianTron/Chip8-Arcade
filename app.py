ebb129e8528f55ecc71257ac091f7283b00179dc
Anthony Feeney <VigilanteNighthawk@gmail.com>
2021-02-20 18:16:46 -0500

    Finished basic configuration of flask-security.  Login and logout work with test user

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_security.utils import hash_password
+from sqlalchemy.sql.functions import user
 from models import db,User,Role
 from flask_migrate import Migrate
 from flask_security import Security,SQLAlchemyUserDatastore
@@ -10,6 +12,18 @@ db.init_app(app)
 migrate = Migrate(app,db)
 user_datastore = SQLAlchemyUserDatastore(db,User, Role)
 security = Security(app,user_datastore)
+
+#create test user
+#TODO remove once signup is created
+@app.before_first_request
+def create_test_user():
+    role = user_datastore.find_or_create_role(name='Game Developer',role='game_dev',description="A chip 8 game developer that can add, edit, and remove their own chip8 games and supply game specific configurations for the chip 8 virtual machine.")
+    if not user_datastore.find_user(name="test_dev"):
+        user = user_datastore.create_user(name='test_dev',email='test_dev@test.com',password=hash_password('password'))
+        user_datastore.add_role_to_user(user,role)
+    db.session.commit()
+
+
 
 
 if __name__ == "__main__":
