a760592958456b77609bdd82d7b7a7d1247b9431
Anthony Feeney <VigilanteNighthawk@gmail.com>
2021-02-20 16:26:17 -0500

    Initial commit of app.py. No routes, only basic setup

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from models import db,User,Role
+from flask_security import Security,SQLAlchemyUserDatastore
+
+app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
+
+db.init_app(app)
+user_datastore = SQLAlchemyUserDatastore(db,User, Role)
+security = Security(app,user_datastore)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
