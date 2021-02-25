d876ac8106306f85fdc229e7de653851dbab2e44
Anthony Feeney <VigilanteNighthawk@gmail.com>
2021-02-25 18:12:47 -0500

    Have moved saving game files into an event callback for insert and moved siginificant portions of functionality into a mixin.

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
+from abc import abstractmethod
+from operator import attrgetter
+from flask_sqlalchemy import SQLAlchemy,event
 from flask_security.models import fsqla_v2 as fsqla
 from sqlalchemy.orm import backref
 from sqlalchemy.ext.hybrid import hybrid_property
@@ -11,6 +13,27 @@ roles_users = db.Table('roles_users',
     db.Column('user_id',db.Integer,db.ForeignKey('user.id')),
     db.Column('role_id',db.Integer,db.ForeignKey('role.id'))
 )
+
+class FileSaveMixin:
+    file = None
+    
+    @property
+    def path(self):
+        return os.path.join(db.get_app().config['UPLOAD_FOLDER'],str(self.filename))
+
+    @abstractmethod
+    def save(self):
+        pass
+
+@event.listens_for(FileSaveMixin,'before_insert',propagate=True)
+def generate_filename(mapper,connection,target):
+    target.filename = uuid.uuid4().hex
+           
+@event.listens_for(FileSaveMixin,'after_insert',propagate=True)
+def save_file(mapper,connection,target):
+    target.save()
+
+
 
 class User(db.Model,fsqla.FsUserMixin):
 
@@ -28,29 +51,22 @@ class Role(db.Model,fsqla.FsRoleMixin):
     role = db.Column(db.String(80),unique=True)
     description = db.Column(db.Text)
 
-class Game(db.Model):
+class Game(db.Model,FileSaveMixin):
     id = db.Column(db.Integer,primary_key=True)
     user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
     title = db.Column(db.String(255),nullable=False)
     description = db.Column(db.Text)
     filename = db.Column(db.String(255),nullable=False)
     user = db.relationship('User',backref=db.backref('games',lazy='dynamic'))
-    
-    #return file path
-    @hybrid_property
-    def file(self):
-        print(type(db.get_app().config['UPLOAD_FOLDER']))
-        return os.path.join(db.get_app().config['UPLOAD_FOLDER'],str(self.filename))
-    
-    #save the file when set as hex stream
-    @file.setter
-    def file(self,file_stream):
-        binary = file_stream
-        hex = binary.hex()
-        self.filename = uuid.uuid4().hex
-        path = os.path.join(db.get_app().config['UPLOAD_FOLDER'],str(self.filename))
-        with open(path,'w') as writer:
+
+    def save(self):
+        hex = self.file.hex()
+        with open(self.path,'w') as writer:
             writer.write(hex)
+
+    
+
+
 
 
 
