991c7061c95fbdf742500c1f9b6ef92a39da242c
Anthony Feeney <VigilanteNighthawk@gmail.com>
2021-03-18 14:52:41 -0400

    Modified emulator to handle case in which the json configuration is loaded but contains errors from the server.

--- static/javascript/emulator_player.js
+++ static/javascript/emulator_player.js
@@ -9,6 +9,9 @@ fetch(gameUrl).then(
     async function(response){
     if(response.ok){
         const config = await response.json();
+        if(config.hasOwnProperty('error')){
+            throw new Error('Game could not be found on the server.');
+        }
         const keys = config['key_config'];
         emulator.loadKeyMap(keys);
         return loadGame(emulator,config['chip8_font'],config['super_chip_font'],config['rom']);
